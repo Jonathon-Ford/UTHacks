69f9f050abe9a4f0953ccca18c03ab645a5c1b6c
Jonathon-Ford <61033062+Jonathon-Ford@users.noreply.github.com>
2021-03-27 18:19:30 -0400

    logout option available

--- newsRate/newsRate/settings.py
+++ newsRate/newsRate/settings.py
@@ -126,3 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = "login"
+LOGIN_REDIRECT_URL = "profile"
+LOGOUT_REDIRECT_URL = "home"
