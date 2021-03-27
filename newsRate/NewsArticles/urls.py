b66680a7ebeec8d23753198452e157b4a45d3fa4
Bear <baokhanh413@gmail.com>
2021-03-27 15:26:59 -0400

    NewsArticles

--- newsRate/NewsArticles/urls.py
+++ newsRate/NewsArticles/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import article_list
 
+from . import admin
+from .views import article_list
+from ..Pages.views import home_view
 
 urlpatterns = [
-    path('', article_list, name='article-list')
+    path('admin/', admin.site.urls),
+    path('', article_list, name='article-list'),
+    path('home/', home_view, name='home'),
 ]
