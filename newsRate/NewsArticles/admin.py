95d902df03a968389a1fab07d06fe4391b7f672f
Bear <baokhanh413@gmail.com>
2021-03-27 16:52:55 -0400

    trying to fix this

--- newsRate/NewsArticles/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import ArticleRating, Article
-
-admin.site.register(ArticleRating)
-admin.site.register(Article)
--- newsRate/NewsArticles/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import article_list
-
-app_name = 'NewsArticles'
-urlpatterns = [
-    path('', article_list, name='article-list'),
-]
