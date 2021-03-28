1d9d9ea972eb8c1dc02d0f59552b36475c95f5be
Jonathon-Ford <61033062+Jonathon-Ford@users.noreply.github.com>
2021-03-28 03:06:49 -0400

    .

--- newsRate/NewsArticles/admin.py
+++ newsRate/NewsArticles/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
  
  
 from .models import Articles, ArticlesRating
+
+class ArticlesAdmin(admin.ModelAdmin):
+    list_display = ("title", "author",)
  
-admin.site.register(Articles)
+admin.site.register(Articles, ArticlesAdmin)
 admin.site.register(ArticlesRating)
