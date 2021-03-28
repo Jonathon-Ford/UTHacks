dc6583c0d1ec1ff5b0cff329de18718113376575
Jonathon-Ford <61033062+Jonathon-Ford@users.noreply.github.com>
2021-03-28 08:28:52 -0400

    .

--- newsRate/NewsArticles/models.py
+++ newsRate/NewsArticles/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Articles(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
-    rating = models.FloatField()
+    url = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title 
@@ -15,9 +15,10 @@ class ArticlesRating(models.Model):
     ratedDate = models.DateTimeField()
     ReviewTitle = models.CharField(max_length=100)
     ArticlesTitle = models.ForeignKey(Articles, on_delete=models.CASCADE)
+    rating = models.FloatField()
     review = models.TextField(default='Write you review here')
     ratedDate = models.DateTimeField()
     
     def __str__(self):
-        return "%s %s" % (self.ReviewTitle, self.review)
+        return self.ReviewTitle
 
