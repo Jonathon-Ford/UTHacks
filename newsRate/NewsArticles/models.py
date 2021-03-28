3de215b5f72aff2c4ede0d3b92d810c73272674e
Bear <baokhanh413@gmail.com>
2021-03-28 01:27:01 -0400

    Linked Rating to Articles

--- newsRate/NewsArticles/models.py
+++ newsRate/NewsArticles/models.py
@@ -1,11 +1,23 @@
 from django.conf import settings
 from django.db import models
  
- 
+
 class Articles(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     rating = models.FloatField()
-    review = models.TextField(default=' ')
+
+    def __str__(self):
+        return self.title 
+
+
+class ArticlesRating(models.Model):
     ratedDate = models.DateTimeField()
- 
+    ReviewTitle = models.CharField(max_length=100)
+    ArticlesTitle = models.ForeignKey(Articles, on_delete=models.CASCADE)
+    review = models.TextField(default='Write you review here')
+    ratedDate = models.DateTimeField()
+    
+    def __str__(self):
+        return "%s %s" % (self.ReviewTitle, self.review)
+
